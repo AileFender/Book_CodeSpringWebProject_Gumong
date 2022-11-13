2fec7378d95bd434a4c915dd8a52a92f214a64d5
LHS <ehs1865@nate.com>
2022-11-13 19:30:29 +0900

    - JUnit Test 시 initializationError 발생으로 버전 다운그레이드 - p194 까지 완료 (Read)

--- src/main/java/org/zerock/mapper/BoardMapper.java
+++ src/main/java/org/zerock/mapper/BoardMapper.java
@@ -10,5 +10,11 @@ public interface BoardMapper {
 //	@Select
 //	("SELECT * FROM TBL_BOARD WHERE BNO > 0")
 	public List<BoardVO> getList();
+	
+	public void insert (BoardVO board);
+	
+	public void insertSelectKey(BoardVO board);
+	
+	public BoardVO read(Long bno);
 
 }
--- src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -1,12 +1,11 @@
 package org.zerock.mapper;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -25,4 +24,38 @@ public class BoardMapperTests {
 		mapper.getList().forEach(board -> log.info(board));
 	}
 
+	@Test
+	public void testInsert() {
+		
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("NewbiE");
+		
+		mapper.insert(board);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testInsertSelectKey() {
+		
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글 Select Key");
+		board.setContent("새로 작성하는 내용 Select Key");
+		board.setWriter("NewbiE");
+		
+		mapper.insertSelectKey(board);
+		
+		log.info(board);
+	}
+	
+	@Test
+	public void testRead() {
+		
+		// 존재하는 게시물 번호로 테스트
+		BoardVO board = mapper.read(5L);
+		
+		log.info(board);
+	}
 }
